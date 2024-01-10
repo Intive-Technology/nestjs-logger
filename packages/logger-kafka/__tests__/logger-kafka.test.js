c7da0a1bd6d29d531b03db25203768ce9d74a0f2
harshad-intive <147067993+harshad-intive@users.noreply.github.com>
2024-01-10 15:30:17 +0530

    chore: add licence

--- packages/logger-kafka/__tests__/logger-kafka.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const loggerKafka = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(loggerKafka(), 'Hello from loggerKafka');
-console.info('loggerKafka tests passed');
--- packages/logger-pg/__tests__/logger-pg.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const loggerPg = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(loggerPg(), 'Hello from loggerPg');
-console.info('loggerPg tests passed');
